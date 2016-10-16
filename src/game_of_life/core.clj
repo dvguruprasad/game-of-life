6985d55fecfb276021b00ae1972e31a3b2d2de57
Guru <dv.guruprasad@gmail.com>
2016-10-17 00:09:27 +0530

    minor refactoring

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,60 +1,57 @@
 (ns game-of-life.core)
 
-(defn- to-cells [points status]
-  (set (map (fn [point] {:point point :status status}) points)))
-
-(defn- to-points [cells status]
-  (set (map (fn [cell] (:point cell)) (filter #(= status (:status %)) cells))))
+(def deltas (range -1 2))
 
 (defn- is-alive? [cell]
   (= :alive (:status cell)))
 
+(defn- to-live-cells [points]
+  (set (map (fn [point] {:point point :status :alive}) points)))
+
+(defn- filter-live [cells]
+  (filter is-alive? cells))
+
+(defn- live-cell-points [cells]
+  (set (map (fn [cell] (:point cell)) (filter-live cells))))
+
 (defn- die [cell]
-  {:point (:point cell) :status :dead})
+  (assoc cell :status :dead))
 
 (defn- live [cell]
-  {:point (:point cell) :status :alive})
+  (assoc cell :status :alive))
 
 (defn- live-neighbour-count [neighbours]
-  (count (filter is-alive? neighbours)))
+  (count (filter-live neighbours)))
 
 (defn- is-alive-at? [point live-cells]
   (some #(= {:point point :status :alive}  %) live-cells))
 
-(defn- cell-status [point live-cells]
-  (if (is-alive-at? point live-cells) :alive :dead))
+(defn- create-cell [point live-cells]
+  {:point point
+   :status (if (is-alive-at? point live-cells) :alive :dead)})
 
-(defn- neighbouring-cell [neighbouring-point live-cells]
-  {:point neighbouring-point
-   :status (cell-status neighbouring-point live-cells)})
+(defn- neighbouring-points [point]
+  (for [x deltas y deltas :when (not= 0 x y)]
+    (map + point [x y])))
 
-(defn- neighbouring-cells [cell live-cells]
-  (set (let [point (:point cell)
-             deltas (range -1 2)]
-         (for [x deltas y deltas :when (not= 0 x y)] 
-           (let [neighbouring-point (map + point [x y])]
-             (neighbouring-cell neighbouring-point live-cells))
-           ))
-       ))
+(defn- neighbours [cell live-cells]
+  (map #(create-cell % live-cells) (neighbouring-points (:point cell))))
 
 (defn create-generation [live-cells]
-  (reduce
-    (fn [result live-cell] (concat result (neighbouring-cells live-cell live-cells)))
-    #{}
-    live-cells
-    ))
+  (let [add-neighbours
+        (fn [result live-cell](concat result (neighbours live-cell live-cells)))]
+    (reduce add-neighbours #{} live-cells)))
 
 (defn next-generation-cell [cell live-cells]
-  (let [neighbours (neighbouring-cells cell live-cells)
-        live-neighbour-count (live-neighbour-count neighbours)]
+  (let [live-neighbour-count (live-neighbour-count (neighbours cell live-cells))]
     (cond
       (or (< live-neighbour-count 2) (> live-neighbour-count 3)) (die cell)
       (= live-neighbour-count 3) (live cell)
       :else cell)
     ))
 
-(defn next-generation [points]
-  (let [live-cells (to-cells points :alive)
-             generation (create-generation live-cells)]
-         (to-points (map #(next-generation-cell % live-cells) generation) :alive))) 
+(defn next-generation [live-points]
+  (let [live-cells (to-live-cells live-points)
+        generation (create-generation live-cells)]
+         (live-cell-points (map #(next-generation-cell % live-cells) generation)))) 
 
